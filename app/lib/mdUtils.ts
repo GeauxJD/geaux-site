1e254ce4a9ef839146485a1bf37f5cd786a42392
Jeff Diecks <55294502+GeauxJD@users.noreply.github.com>
2024-12-18 17:20:46 -0500

    Update mdUtils.ts

--- app/lib/mdUtils.ts
+++ app/lib/mdUtils.ts
@@ -5,9 +5,8 @@ import matter from 'gray-matter'
 const postsDirectory = path.join(process.cwd(), 'posts')
 
 export function getSortedPostsData() {
-  // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map((fileName) => {
+  const allPostsData = fileNames.map(fileName => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '')
 
@@ -18,34 +17,28 @@ export function getSortedPostsData() {
     // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents)
 
-    // Combine the data with the id
     return {
       id,
-      ...(matterResult.data as { date: string; title: string })
+      ...matterResult.data
     }
   })
-  // Sort posts by date
+
   return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1
-    } else {
-      return -1
-    }
+    if (a.date < b.date) return 1
+    else return -1
   })
 }
 
 export function getPostData(id: string) {
-  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fullPath = path.join(postsDirectory, `${id}.md`) // Changed .mdx to .md
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
   // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents)
 
-  // Combine the data with the id and contentHtml
   return {
     id,
     content: matterResult.content,
-    ...(matterResult.data as { date: string; title: string })
+    ...matterResult.data
   }
 }
-
